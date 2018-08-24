2940939f99c3fb6957e454319b0878274260957f
Arjen Nienhuis <arjen@zorgdoc.nl>
2018-08-24 09:21:32 +0200

    mypy

--- medmij/tests/test_whitelist.py
+++ medmij/tests/test_whitelist.py
@@ -1,5 +1,5 @@
 # pylint: skip-file
-
+from typing import Any, AnyStr
 from unittest import TestCase
 from unittest.mock import patch, MagicMock
 import lxml
@@ -8,7 +8,7 @@ import medmij
 from . import testdata
 
 
-def string_urlopen(response_string) -> Any:
+def string_urlopen(response_string: AnyStr) -> Any:
     response = MagicMock()
     response.__enter__.return_value = response
     response.getcode.return_value = 200
@@ -17,34 +17,32 @@ def string_urlopen(response_string) -> Any:
 
 
 class TestWhitelist(TestCase):
-    def test_parse_ok(self):
+    def test_parse_ok(self) -> None:
         for xml in (testdata.WHITELIST_EXAMPLE_XML,
                     testdata.WHITELIST_EXAMPLE_SINGLE_XML):
             whitelist = medmij.Whitelist(xml)
             self.assertTrue(isinstance(whitelist, medmij.Whitelist))
 
-    def test_parse_invalid_xml(self):
+    def test_parse_invalid_xml(self) -> None:
         with self.assertRaises(lxml.etree.XMLSyntaxError):
             medmij.Whitelist('<kapot')
 
-    def test_parse_xsd_fail(self):
+    def test_parse_xsd_fail(self) -> None:
         for xml in (testdata.WHITELIST_XSD_FAIL1,
                     testdata.WHITELIST_XSD_FAIL2):
             with self.assertRaises(lxml.etree.XMLSyntaxError):
                 medmij.Whitelist(xml)
 
-    def test_whitelist_contains(self):
+    def test_whitelist_contains(self) -> None:
         whitelist = medmij.Whitelist(testdata.WHITELIST_EXAMPLE_XML)
         self.assertIn("rcf-rso.nl", whitelist)
         self.assertNotIn("rcf-rso.nl.", whitelist)
         self.assertNotIn("RFC-RSO.NL", whitelist)
-        self.assertNotIn("", whitelist)
         self.assertNotIn(None, whitelist)
 
-    @patch('urllib.request.urlopen')
-    def test_whitelist_download(self, mock_urlopen):
-        mock_urlopen.return_value = string_urlopen(
-            testdata.WHITELIST_EXAMPLE_XML)
-        url = "https://example.com/MedMij_Whitelist_example.xml"
-        whitelist = medmij.Whitelist.from_url(url)
+    def test_whitelist_download(self) -> None:
+        with patch('urllib.request.urlopen') as mock_urlopen:
+            mock_urlopen.return_value = string_urlopen(
+                testdata.WHITELIST_EXAMPLE_XML)
+            whitelist = medmij.Whitelist.from_url("http://example.com/")
         self.assertIsInstance(whitelist, medmij.Whitelist)
--- medmij/whitelist.py
+++ medmij/whitelist.py
@@ -1,24 +1,24 @@
 """
 Defineert de class Whitelist
 """
+from __future__ import annotations
 import urllib.request
-from typing import AnyStr, Set, Any
+from typing import AnyStr, Set, Optional
 import pkg_resources
 from lxml import etree
 
 class Whitelist:
     """
     Een whitelist zoals beschreven op https://afsprakenstelsel.medmij.nl/
-
     """
     NS = "xmlns://afsprakenstelsel.medmij.nl/whitelist/release2/"
-    _parser: etree.XMLParser
+    _parser: Optional[etree.XMLParser] = None
 
     @classmethod
     def _get_xsd_parser(cls) -> etree.XMLParser:
         if cls._parser is None:
             data = pkg_resources.resource_string(__name__, "whitelist.xsd")
-            xsdxml = etree.XML(data)
+            xsdxml = etree.fromstring(data)
             xsd = etree.XMLSchema(xsdxml)
             cls._parser = etree.XMLParser(schema=xsd)
 
@@ -29,11 +29,11 @@ class Whitelist:
         xml = etree.fromstring(xmldata, parser=parser)
         self._hostnames = self._parse(xml)
 
-    def __contains__(self, key: Any) -> bool:
+    def __contains__(self, key: object) -> bool:
         return key in self._hostnames
 
     @staticmethod
-    def _parse(xml: "etree._Element") -> Set[str]:
+    def _parse(xml: etree.Element) -> Set[str]:
         nss = {'w': Whitelist.NS}
         return set(xml.xpath(f'//w:MedMijNode/text()', namespaces=nss))
 
